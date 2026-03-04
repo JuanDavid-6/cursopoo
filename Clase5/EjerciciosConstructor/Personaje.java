aa2bf63b64e88b08ee992731836d89ad2292be32
JuanDavid-6 <juand.bolanos@campusucc.edu.co>
2026-03-04 01:42:40 +0000

    Bye

---
+++ Clase5/EjerciciosConstructor/Personaje.java
@@ -0,0 +1,18 @@
+package Clase5.EjerciciosConstructor;
+
+public class Personaje {
+
+    // Atributos
+    String nombre;
+    int nivel;
+
+    // Costructor que obliga a dar un nombre al crear el personaje
+    public Personaje(String nombre, int nivel) {
+        this.nombre = nombre;
+        this.nivel = nivel; // Todos empiezan en nivel 1 por defecto
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Heroe : " + nombre + " | Nivel : " + nivel);
+    }
+}
